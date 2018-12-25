ce3d0870ccf08c84337d16cd40c94d3d9c11e1c5
coooopeeeer <miminashicooper@gmail.com>
2018-12-24 16:03:57 -0800

    Add footer elements

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div id="wrapper">© NATSUMI KOBAYASHI 2018</div>
+  </footer>
+  <script src="./js/jquery-3.3.1.min.js"></script>
+  <script src="./js/main.js"></script>
+  <?php wp_footer();?>
+</body>
+
+</html>
