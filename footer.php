00b8b18ae56fc097b16d3119c5e331a8005a0bc4
coooopeeeer <miminashicooper@gmail.com>
2018-12-27 17:06:17 -0800

    Fix error

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div id="wrapper">© NATSUMI KOBAYASHI 2018</div>
-  </footer>
-  <script src="./js/jquery-3.3.1.min.js"></script>
-  <script src="./js/main.js"></script>
-  <?php wp_footer();?>
-</body>
-
-</html>
