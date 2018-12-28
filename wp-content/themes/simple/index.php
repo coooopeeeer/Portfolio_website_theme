71560408d170fa9edc22182009d81b3bde939698
coooopeeeer <miminashicooper@gmail.com>
2018-12-27 19:10:27 -0800

    Add code

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -1,3 +1,275 @@
 <?php get_header(); ?>
 
+<section id="main">
+    <div id="wrapper">
+      <div class="intro">
+        <h1>
+          Hi There
+          <i class="fas fa-hand-paper" data-fa-transform="rotate-30"></i> My name is
+          <strong>NATSUMI</strong>,
+          <br />a
+          <strong>designer</strong> /
+          <strong>front-end developer</strong>
+          <br />based in Vancouver, Canada
+        </h1>
+        <ul>
+          <li>
+            <a href="https://dribbble.com/coooopeeeer" target="_blank">
+              <i class="fab fa-dribbble"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/coooopeeeer" target="_blank">
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/miminashi_design/" target="_blank">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/coooopeeeer/" target="_blank">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="toDown">
+        <a href="#about"></a>
+      </div>
+    </div>
+  </section>
+  <section id="about">
+    <div id="wrapper">
+      <h2>
+        <span>WHO I AM ?</span>
+      </h2>
+      <div class="bio">
+        <div class="aboutImg">
+          <img src="./img/img_profile.png" alt="Natsumi Kobayashi" />
+        </div>
+        <div class="aboutSkill">
+          <p>
+            Hi there! I am Natsumi Kobayashi, a Japanese web / graphic designer and front-end developer based in Vancouver. I am currently
+            studying Web &amp; Mobile Application Development at CICCC. Before I came here, I worked as a web designer /
+            front-end developer over 4 years in Tokyo. I am passionate about creating design that makes users excited.
+          </p>
+          <h3>PROFESSIONAL SKILLS I HAVE</h3>
+          <ul>
+            <li>
+              <span class="skill">Web design</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+            </li>
+            <li>
+              <span class="skill">Sass</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+            </li>
+            <li>
+              <span class="skill">React</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers"></span>
+              <span class="numbers"></span>
+            </li>
+            <li>
+              <span class="skill">Wordpress</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers"></span>
+            </li>
+            <li>
+              <span class="skill">JavaScript</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers"></span>
+            </li>
+            <li>
+              <span class="skill">HTML5</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+            </li>
+            <li>
+              <span class="skill">CSS3</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+            </li>
+            <li>
+              <span class="skill">Graphic design</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers"></span>
+            </li>
+          </ul>
+          <h3>DESIGN TOOLS I USE</h3>
+          <ul>
+            <li>
+              <span class="skill">Photoshop</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+            </li>
+            <li>
+              <span class="skill">Illustrator</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers"></span>
+            </li>
+            <li>
+              <span class="skill">Adobe XD</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+            </li>
+            <li>
+              <span class="skill">Sketch</span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers better"></span>
+              <span class="numbers"></span>
+            </li>
+          </ul>
+          <h3>THIRSTY TO LEARN MORE</h3>
+          <ul>
+            <li>
+              <span class="skill">Vue.js</span>
+            </li>
+            <li>
+              <span class="skill">AngularJS</span>
+            </li>
+            <li>
+              <span class="skill">PHP</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="works">
+    <div id="wrapper">
+      <h2>
+        <span>MY WORKS</span>
+      </h2>
+      <div class="works odd">
+        <div class="description">
+          <p class="ttl">WakeMeApp</p>
+          <span class="tag">design</span>
+          <span class="tag">Sketch</span>
+          <span class="tag">mobile</span>
+          <span class="tag">app</span>
+          <span class="tag">Android</span>
+          <p class="detail">
+            WakeMeApp is Android application for those who are often late for school, work and anything you should be there before or
+            on time. This app lets you wake up on time and be able to check if your friends get up or oversleep.
+            <br />
+            <strong>NEVER let anyone call you a SLEEPY HEAD!</strong>
+            <br>
+            <span class="addition">Coming soon on Google Play store!</span>
+          </p>
+          <div class="btns">
+            <p>
+              <a href="./wake-me-app.html" class="btn">View FULL design</a>
+            </p>
+            <!-- <a href="" target="_blank">
+              <img src="./img/btn_android.png" alt="GET IT ON Google Play" />
+            </a> -->
+          </div>
+        </div>
+        <div class="thumbnail">
+          <img src="./img/thumb_wakemeapp.png" alt="WakeMeApp" />
+        </div>
+      </div>
+      <div class="works even">
+        <div class="thumbnail">
+          <img src="./img/thumb_wakemeapp.png" alt="WakeMeApp" />
+        </div>
+        <div class="description">
+          <p class="ttl">WakeMeApp</p>
+          <span class="tag">mobile</span>
+          <span class="tag">app</span>
+          <span class="tag">design</span>
+          <span class="tag">Android</span>
+          <p class="detail">
+            WakeMeApp is Android application for those who are often late for school, work and anything you should be there before or
+            on time. This app lets you wake up on time and be able to check if your friends get up or sleep over.
+            <br />
+            <strong>NEVER let anyone call you a SLEEPY HEAD!</strong>
+          </p>
+          <div class="btns">
+            <p>
+              <a href="./wake-me-app.html" class="btn">View FULL design</a>
+            </p>
+            <a href="" target="_blank">
+              <img src="./img/btn_android.png" alt="GET IT ON Google Play" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="contact">
+    <div id="wrapper">
+      <h2>
+        <span>GET IN TOUCH WITH ME</span>
+      </h2>
+      <p>
+        Thank you for visiting my website :)
+        <br />I’m currently looking for a job position as a web designer / front-end developer.
+        <br />Please contact me and I will get back to you as soon as possible!
+      </p>
+      <a href="mailto:miminashicooper@gmail.com" class="btn">DROP ME A LINE :)</a>
+      <p>You can also find me on following SNS!</p>
+      <ul>
+        <li>
+          <a href="https://dribbble.com/coooopeeeer" target="_blank">
+            <i class="fab fa-dribbble"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/coooopeeeer" target="_blank">
+            <i class="fab fa-github"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/miminashi_design/" target="_blank">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/coooopeeeer/" target="_blank">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+
 <?php get_footer(); ?>
