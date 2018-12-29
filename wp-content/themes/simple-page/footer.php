e01261ce8bb0960252ab510d6b3f25e94bf5f119
coooopeeeer <miminashicooper@gmail.com>
2018-12-28 16:33:27 -0800

    Add php file for footer

---
+++ wp-content/themes/simple-page/footer.php
@@ -0,0 +1,8 @@
+    <footer>
+      <div id="wrapper">© NATSUMI KOBAYASHI 2018</div>
+    </footer>
+    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/main.js"></script>
+    <?php wp_footer(); ?>
+  </body>
+</html>
