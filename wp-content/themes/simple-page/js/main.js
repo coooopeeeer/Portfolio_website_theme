b8b6133a09d036e08b174875bbe17b2d9341fd18
coooopeeeer <miminashicooper@gmail.com>
2018-12-30 14:10:01 -0800

    Add js files

---
+++ wp-content/themes/simple-page/js/main.js
@@ -0,0 +1,11 @@
+// smooth scroll
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var speed = 1000;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body, html').animate({ scrollTop: position }, speed, 'swing');
+        return false;
+    });
+});
