493d3d8f0972da31a62b124481cfa179b87422a1
coooopeeeer <miminashicooper@gmail.com>
2019-01-04 16:49:17 -0800

    Add console.log

--- wp-content/themes/simple-page/functions.php
+++ wp-content/themes/simple-page/functions.php
@@ -1,4 +1,5 @@
 <?php function script_load() {
     wp_enqueue_script('script_load', get_template_directory_uri().'js/main.js', array('jquery'), '1.0.0', true);
 }
-add_action( 'wp_enqueue_script', 'script_load' ); ?>
+add_action( 'wp_enqueue_script', 'script_load' );
+console.log("Hello World"); ?>
