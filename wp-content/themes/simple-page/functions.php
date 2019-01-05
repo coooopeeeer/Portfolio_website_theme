da3cd092d7fd206a2fcb0a37cdf88f4395ddc8f2
coooopeeeer <miminashicooper@gmail.com>
2019-01-04 17:11:25 -0800

    Fix path

--- wp-content/themes/simple-page/functions.php
+++ wp-content/themes/simple-page/functions.php
@@ -1,6 +1,4 @@
-<?php 
-    function script_load() {
-        wp_enqueue_script('script-load', get_template_directory_uri().'js/main.js', array('jquery'), date('U'));
-    }
-    add_action( 'wp_enqueue_scripts', 'script_load' ); 
-?>
+<?php function script_load() {
+    wp_enqueue_script('script_load', get_template_directory_uri().'/js/main.js', array('jquery'), '1.0.0', true);
+}
+add_action( 'wp_enqueue_script', 'script_load' ); ?>
