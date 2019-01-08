549a16c15c93377026dd45f85388bbca4b078e42
coooopeeeer <miminashicooper@gmail.com>
2019-01-08 15:14:16 +0900

    Fix error

--- wp-content/themes/simple-page/functions.php
+++ wp-content/themes/simple-page/functions.php
@@ -1,4 +1,5 @@
 <?php function script_load() {
-    wp_enqueue_script('script_load', get_template_directory_uri().'/js/main.js', array('jquery'), '1.0.0', true);
+    var_dump("a");
+    //wp_enqueue_script('script_load', get_template_directory_uri().'/js/main.js', array('jquery'), '1.0.0', true);
 }
 add_action( 'wp_enqueue_script', 'script_load' ); ?>
