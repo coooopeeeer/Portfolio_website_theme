bb7b1e521599479053f0d9b22cfaca366ebd84b6
coooopeeeer <miminashicooper@gmail.com>
2019-01-19 23:46:54 +0900

    Add new directory

---
+++ wp-content/themes/simple-page/projects/wake-me-app.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<section id="projectInfo">
+    <div id="wrapper">
+        <h1>WakeMeApp</h1>
+        <h2>Android application</h2>
+        <ul>
+            <li>Sketch</li>
+        </ul>
+    </div><!-- End div.wrapper -->
+</section>
+<section id="projectDetail">
+    <div>
+        <h3>ABOUT</h3>
+        <p>There were a lot of things that I had never done before in making this app. Also integrating sketch UI designs into iOS apps was more difficult than I expected.</p>
+    </div>
+    <div>
+        <h3>LOGIN</h3>
+        <img src="" alt="">
+    </div>
+</section>
+
+<?php get_footer(); ?>
