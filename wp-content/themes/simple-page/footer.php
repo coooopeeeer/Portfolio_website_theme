433c14fdf520e430732a902a6787bc366283bcce
coooopeeeer <miminashicooper@gmail.com>
2019-01-19 10:44:25 +0900

    Fix error to read js file

--- wp-content/themes/simple-page/footer.php
+++ wp-content/themes/simple-page/footer.php
@@ -1,6 +1,8 @@
     <footer>
       <div id="wrapper">© NATSUMI KOBAYASHI 2018</div>
     </footer>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
     <?php wp_footer(); ?>
   </body>
 </html>
--- wp-content/themes/simple-page/functions.php
+++ wp-content/themes/simple-page/functions.php
@@ -1,5 +1,9 @@
-<?php function script_load() {
+<?php
+/*
+function script_load() {
     var_dump("a");
-    //wp_enqueue_script('script_load', get_template_directory_uri().'/js/main.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('script_load', get_template_directory_uri().'/js/main.js', array('jquery'), '1.0.0', true);
 }
-add_action( 'wp_enqueue_script', 'script_load' ); ?>
+add_action( 'wp_enqueue_script', 'script_load' );
+*/
+?>
