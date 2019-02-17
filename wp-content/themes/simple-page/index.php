1b996629858fb4b8c87d39e04c75d99e1ab47b56
coooopeeeer <miminashicooper@gmail.com>
2019-02-16 18:18:59 -0800

    Add icon

--- wp-content/themes/simple-page/index.php
+++ wp-content/themes/simple-page/index.php
@@ -320,7 +320,7 @@
             <a href="./portfolio.html" class="btn">View FULL description</a>
           </p> -->
           <p>
-            <a href="https://github.com/coooopeeeer/Portfolio_website_theme/tree/master/wp-content/themes/simple-page" class="btn github" target="_blank" rel="noopener noreferrer">See on Github</a>
+            <a href="https://github.com/coooopeeeer/Portfolio_website_theme/tree/master/wp-content/themes/simple-page" class="btn github" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i>See on Github</a>
           </p>
         </div>
       </div>
