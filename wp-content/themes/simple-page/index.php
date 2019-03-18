1ba0d039cd2b77f736b65d7a54cfc6ee74cf5a02
coooopeeeer <miminashicooper@gmail.com>
2019-03-18 09:53:30 -0700

    Fix error

--- wp-content/themes/simple-page/index.php
+++ wp-content/themes/simple-page/index.php
@@ -203,7 +203,7 @@
         </p>
         <div class="btns">
           <!-- <p>
-            <a href="<?php echo home_url('/index.php/sample-page/'); ?>" class="btn">View FULL design</a>
+            <a href="<?php echo home_url('/index.php/sample-page/'); ?>" class="btn">View FULL detail</a>
           </p> -->
           <p>
             <a href="https://github.com/coooopeeeer/Portfolio_website_theme/tree/master/wp-content/themes/simple-page" class="btn github" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i>See on Github</a>
